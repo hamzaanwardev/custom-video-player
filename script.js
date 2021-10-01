7d34c2df2184a600aaefabf7c6281005ef3832a8
Hamza Anwar <88555920+hamzaanwardev@users.noreply.github.com>
2021-10-02 01:38:50 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const play = document.getElementById('play');
 const stop = document.getElementById('stop');
 const progress = document.getElementById('progress');
 const timestamp = document.getElementById('timestamp');
+
+// Event listeners
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgress);
